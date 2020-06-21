cb9c4203c822f511bca80fe90c181e6718e21661
Trim Bresilla <trim.bresilla@gmail.com>
2020-06-21 15:25:43 +0200

    FIX: nix-shell

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
+#include <cplate/classic.h>
 #include <fmt/format.h>
 #include <string>
-#include <cplate/classic.h>
 
 int main() {
     std::string s = fmt::format( "I'd rather be {1} than {0}.", "right", "happy" );
